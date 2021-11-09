d1109ef7d0d90849c8330da65df69f8d90758377
senchaid <etercian@gmail.com>
2021-11-09 09:46:51 +0000

    Add files via upload

--- parsedate.py
+++ parsedate.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import datetime as dt
 import pickle
 
 def parse_date(dt):
@@ -8,6 +9,12 @@ def parse_date(dt):
 with open('C:\\Users\\Anna\\aws\\video_N.log') as file:
 	lines = file.readlines()
 	lines = [line.rstrip() for line in lines]
+
+
+with open('C:\\Users\\Anna\\aws\\offset_N.log') as file1:
+	offset = file1.readlines()
+	offset = [line.rstrip() for line in offset]
+
 
 N={}
 N_offset={}
@@ -20,12 +27,32 @@ for line in lines:
 		tmp=[parse_date(s[1])]
 	N[s[0]]=tmp
 
+
+for line in offset:
+	s=line.split('\t')
+	delta1=dt.timedelta(minutes=int(s[1].split(':')[0]), seconds=int(s[1].split(':')[1]))
+	if (len(s)>2):
+		delta2=dt.timedelta(minutes=int(s[2].split(':')[0]), seconds=int(s[2].split(':')[1]))
+		tmp=[N[s[0]][0]+delta1, N[s[0]][1]+delta2]
+	else:
+		tmp=[N[s[0]][0]+delta1]
+	N_offset[s[0]]=tmp
+
 with open('N_starttime.pickle', 'wb') as handle:
 	pickle.dump(N, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('N_endtime.pickle', 'wb') as handle:
+	pickle.dump(N_offset, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 with open('C:\\Users\\Anna\\aws\\video_C.log') as file:
 	lines = file.readlines()
 	lines = [line.rstrip() for line in lines]
+
+with open('C:\\Users\\Anna\\aws\\offset_C.log') as file1:
+	offset = file1.readlines()
+	offset = [line.rstrip() for line in offset]
+
+
 
 C={}
 C_offset={}
@@ -38,8 +65,25 @@ for line in lines:
 		tmp=[parse_date(s[1])]
 	C[s[0]]=tmp
 
+
+for line in offset:
+	s=line.split('\t')
+	delta1=dt.timedelta(minutes=int(s[1].split(':')[0]), seconds=int(s[1].split(':')[1]))
+	if (len(s)>2):
+		delta2=dt.timedelta(minutes=int(s[2].split(':')[0]), seconds=int(s[2].split(':')[1]))
+		tmp=[C[s[0]][0]+delta1, C[s[0]][1]+delta2]
+	else:
+		tmp=[C[s[0]][0]+delta1]
+	C_offset[s[0]]=tmp
+
+
+with open('C_endtime.pickle', 'wb') as handle:
+	pickle.dump(C_offset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 print(C)
 print(N)
+print(N_offset)
+print(C_offset)
 
 with open('C_starttime.pickle', 'wb') as handle:
 	pickle.dump(C, handle, protocol=pickle.HIGHEST_PROTOCOL)
